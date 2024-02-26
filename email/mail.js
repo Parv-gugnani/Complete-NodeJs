310af4844bf6f06194456cff47ca4999e1cec99f
Parv-gugnani <9765.parv@gmail.com>
2024-02-26 17:28:41 +0530

    failed

--- email/mail.js
+++ email/mail.js
@@ -1,4 +1,25 @@
 var http = require("http");
 var nodemailer = require("nodemailer");
 
-var transporter;
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "process.env.mail",
+    pass: "process.env.pass",
+  },
+});
+
+var mailOptions = {
+  from: "process.env.mail",
+  to: "process.env.myself",
+  subject: "Hello from another side",
+  text: "This is testing of email",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email Sent Successfully" + info.response);
+  }
+});
