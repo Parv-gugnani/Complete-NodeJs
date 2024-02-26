3b730e13c6ca254e9e07542bf97b7b025f807e4c
Parv-gugnani <9765.parv@gmail.com>
2024-02-26 17:06:26 +0530

    Getting started

---
+++ Get started/myfirst.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello world!");
+  })
+  .listen(8080);
+
+// The code tells the computer to write "Hello World!" if anyone (e.g. a web browser) tries to access your computer on port 8080.
