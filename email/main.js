3dde29a51cef2703ae4b7993f8f64178369460b8
Parv-gugnani <9765.parv@gmail.com>
2024-09-19 17:05:28 +0530

    mailgun

--- email/main.js
+++ email/main.js
@@ -44,3 +44,24 @@ app.post("/send-email", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+/*
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+
+const mailgun = new Mailgun(formData);
+
+const mg = mailgun.client({
+   username: "api",
+   key: process.env.MAILGUN_API_KEY,
+});
+exports.sendMail = (req, res) => {
+   const { toEmail, fromEmail, subject, message } = req.body;
+
+      mg.messages.create(process.env.MAILGUN_DOMAIN, {
+      from: fromEmail,
+      to: [toEmail],
+      subject: subject,
+      text: message,
+   });
+}; */
