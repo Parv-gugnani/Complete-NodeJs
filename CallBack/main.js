3d2c45adc0c0fea2b151b2d23eacfd4448822a3a
Parv-gugnani <9765.parv@gmail.com>
2024-09-19 17:03:27 +0530

    email working

--- CallBack/main.js
+++ CallBack/main.js
@@ -1,10 +1,25 @@
-var fs = require("fs");
-var data = fs.readFileSync("input.txt");
+//Blocking Code
+// var fs = require("fs");
+// var data = fs.readFileSync("input.txt");
 
-console.log(data.toString());
+// console.log(data.toString());
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log("The number is " + i);
+//   i++;
+// }
+
+//Non Blocking
+var fs = require("fs");
+fs.readFile("input.txt", function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
 
 let i = 1;
 while (i <= 5) {
   console.log("The number is " + i);
   i++;
 }
+//it will first loop then the input txt will be printed
