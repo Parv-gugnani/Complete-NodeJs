aaa2af751efae0ad9f012b0b163cbcca35b647d6
Parv-gugnani <9765.parv@gmail.com>
2024-09-23 17:10:17 +0530

    pckg

--- NodeJs Basic/Restfull Api/index.js
+++ NodeJs Basic/Restfull Api/index.js
@@ -13,3 +13,21 @@ app.get("/", function (req, res) {
 var server = app.listen(5000, function () {
   console.log("Express App running at localhost khud dhund le 😡");
 });
+
+app.delete("/:id", function (req, res) {
+  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
+    data = JSON.parse(data);
+    var id = "user" + req.params.id;
+    var user = data[id];
+    delete data["user" + req.params.id];
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.get("/:id", function (req, res) {
+  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
+    var users = JSON.parse(data);
+    var user = users["user" + req.params.id];
+    res.end(JSON.stringify(user));
+  });
+});
