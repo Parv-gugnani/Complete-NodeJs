9bf7abc981d818e8238c754676b34436d30c32ef
Parv-gugnani <9765.parv@gmail.com>
2024-09-25 17:21:28 +0530

    inserting

---
+++ NodeJs SQL/insert into/index.js
@@ -0,0 +1,48 @@
+// var mysql = require("mysql");
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "paswords",
+//   database: "mydb",
+// });
+
+// var data = ["Ravi", 25, 25000];
+
+// var qry = `INSERT INTO employee (name, age, salary) VALUES(?,?,?);`;
+
+// // var qry = `INSERT INTO employee (name, age, salary) VALUES("Ravi", 25, 25000);`;
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query(qry, data, function (err, result) {
+//     if (err) throw err;
+//     console.log("New Record inserted successfully");
+//   });
+
+//   con.end();
+// });
+
+var mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "mydb",
+});
+var data = [
+  ["Ravi", 25, 25000],
+  ["Anil", 26, 30000],
+  ["Meena", 26, 27000],
+];
+var qry = `INSERT INTO employee (name, age, salary) VALUES(?,?,?);`;
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  for (var i = 0; i < data.length; i++) {
+    con.query(qry, data[i], function (err, result) {
+      if (err) throw err;
+      console.log("New Record inserted successfully");
+    });
+  }
+  con.end();
+});
